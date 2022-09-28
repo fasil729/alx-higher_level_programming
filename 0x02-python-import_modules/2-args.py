09c30344e097de617378c2c12aa38b434c15b7f9
fasika <fasikafikadu38@gmail.com>
2022-09-28 16:52:13 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print(f'{len(argv)} arguments:')
+    if len(argv) == 1:
+        print(f'{len(argv)} argument:')
+    else:
+        if len(argv) == 0:
+            print(f'{len(argv)} arguments.')
+        else:
+            print(f'{len(argv)} arguments:')
     for i in range(len(argv)):
         print(f"{i+1}: {argv[i]}")
