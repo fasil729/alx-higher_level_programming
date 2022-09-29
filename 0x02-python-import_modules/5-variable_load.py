67b109277464a878f4f0136065fcdbb14b0641f2
fasika <fasikafikadu38@gmail.com>
2022-09-28 18:14:52 -0700

    task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import variable_load_5
-    try:
-        print(variable_load_5.a)
-    except AttributeError:
-        print("there is no such variable")
+    print(variable_load_5.a)
