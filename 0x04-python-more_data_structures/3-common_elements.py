a739fdbe2d55fc09041c6d95b6e1a42d4970d1c4
fasika <fasikafikadu38@gmail.com>
2022-09-29 06:52:54 -0700

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,6 @@
 def common_elements(set_1, set_2):
     common_set = []
     for element in set_1:
-        print(element)
         if element in set_2:
             common_set.append(element)
         else:
