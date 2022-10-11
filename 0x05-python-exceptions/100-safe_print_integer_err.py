df41426272df7ca9fc7099295b18e17eddf88765
fasika <fasikafikadu38@gmail.com>
2022-10-10 23:15:53 -0700

    advanced 1

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as te:
-        print(f"Exception: {te}")
+        sys.stderr.write(f"Exception: {te}\n")
         return False
