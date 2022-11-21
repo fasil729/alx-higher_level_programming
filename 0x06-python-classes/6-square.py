de7001a9277d1c3cf96a6d488bddf67254a5f7a6
fasika <fasikafikadu38@gmail.com>
2022-11-21 06:34:53 -0800

    print using position tuple

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" private instance instatiation with some criteria"""
+
+
+class Square:
+    """instatiation of __size as integer only and greater than 0"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        assert isinstance(value, int), "size must be an integer"
+        assert value >= 0, "size must be >= 0"
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        assert isinstance(value, tuple) and (value[0] >= 0 and value[1] >= 0),\
+                "position must be a tuple of 2 positive integers"
+        self.__position = value
+
+    def area(self):
+        """ retruns the current area """
+        return self.size ** 2
+
+    def my_print(self):
+        for i in range(self.size):
+            print(self.position[0] * ' ' + '#' * self.size)
+        if self.size == 0:
+            print()
