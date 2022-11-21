c550b728c544b0c07d32855ecdb2007974ff0920
fasika <fasikafikadu38@gmail.com>
2022-11-21 05:58:45 -0800

    using @property decorator instead of setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" private instance instatiation with some criteria"""
+
+
+class Square:
+    """instatiation of __size as integer only and greater than 0"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        assert isinstance(value, int), "size must be an integer"
+        assert value >= 0, "size must be >= 0"
+        self.__size = value
+
+    def area(self):
+        """ retruns the current area """
+        return self.size ** 2
