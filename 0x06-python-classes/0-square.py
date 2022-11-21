71932681ae06f10af882fc628460784bce7535b6
fasika <fasikafikadu38@gmail.com>
2022-11-21 05:11:33 -0800

    private attribute

--- 0x06-python-classes/0-square.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-""" creating empty class """
-class Square:
-	""" empety class  """
-	pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" creating  class with private attribute"""
+
+
+class Square:
+    """ class with private attribue  """
+    __size = 0
