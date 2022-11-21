1d513d1560f828627ed974761f0bc2c0ed24ed23
fasika <fasikafikadu38@gmail.com>
2022-11-21 05:40:05 -0800

    private with some assertion

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" private instance instatiation with some criteria"""
+
+
+class Square:
+    """instatiation of __size as integer only and greater than 0"""
+    def __init__(self, size=0):
+        assert isinstance(size, int), "size must be an integer"
+        assert size >= 0, "size must be >= 0"
+        self.__size = size
