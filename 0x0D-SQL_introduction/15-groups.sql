207f8c4009b87b11b76eb29b6456db829bc4dca5
fasika <fasikafikadu38@gmail.com>
2022-11-29 17:01:22 -0800

    using group

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- displaying the records with the same score as labeled with score and number(number of records)
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
