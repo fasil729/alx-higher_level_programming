0c8b14dcc911ea2f658ea7aa82d608605e83f144
fasika <fasikafikadu38@gmail.com>
2022-12-01 01:48:07 -0800

    left join

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- a script that lists all shows, and all genres linked to that show, from the database
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Results must be sorted in ascending order by the show title and genre name
+SELECT t.`title`, g.`name` 
+	FROM `tv_shows` AS t 
+		LEFT JOIN `tv_show_genres` AS s 
+		ON s.`genre_id`= t.`id` 
+		
+		LEFT JOIN `tv_genres` AS g 
+		ON g.id = s.`genre_id`
+	ORDER BY t.title,g.name;
