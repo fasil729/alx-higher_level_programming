6ad8f84f1a55494d3f7ed51fcd46b49fbc44bf9e
fasika <fasikafikadu38@gmail.com>
2022-11-30 18:20:11 -0800

    databse and grant

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- create user AND DATABSE AND GRANT PERMISSION
+CREATE DATABASE
+	IF NOT EXISTS 'hbtn_0d_2'
+CREATE USER
+        IF NOT EXISTS "user_0d_2"@"localhost"
+        IDENTIFIED BY "user_0d_2_pwd";
+GRANT SELECT
+        ON 'hbtn_0d_2'.*
+        TO "user_0d_2"@"localhost";
+
