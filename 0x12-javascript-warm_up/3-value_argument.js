fc39d9aa9d79eeb32d43444e3e28747c4099a800
fasika <fasikafikadu38@gmail.com>
2022-12-19 18:35:30 -0800

    frist argv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 const argv = process.argv;
-if (argv.length === 2) {
+if (!argv[2]) {
   console.log('No argument');
 } else {
   console.log(argv[2]);
