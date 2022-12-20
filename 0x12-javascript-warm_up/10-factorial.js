a6fc965e7fb67a07174347bac4fbabbc52a1360e
fasika <fasikafikadu38@gmail.com>
2022-12-20 03:46:39 -0800

    find factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,13 +3,13 @@ function factorial (a) {
   if (a === 0) {
     return 1;
   }
-  return a * factorial(a-1);
+  return a * factorial(a - 1);
 }
 const process = require('process');
 const argv = process.argv;
 const num = parseInt(argv[2], 10);
-if (num)
+if (num) {
   console.log(factorial(num));
-else
+} else {
   console.log(factorial(0));
-	
+}
