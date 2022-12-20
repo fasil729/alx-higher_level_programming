9138864002d3e63ced3b169823135bbee6a6c498
fasika <fasikafikadu38@gmail.com>
2022-12-20 03:43:44 -0800

    print factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial (a) {
+  if (a === 0) {
+    return 1;
+  }
+  return a * factorial(a-1);
+}
+const process = require('process');
+const argv = process.argv;
+const num = parseInt(argv[2], 10);
+if (num)
+  console.log(factorial(num));
+else
+  console.log(factorial(0));
+	
