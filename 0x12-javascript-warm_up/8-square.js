23fbdfda7268782e0e946e5efccdbd8ca895e00c
fasika <fasikafikadu38@gmail.com>
2022-12-20 03:16:17 -0800

    print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const process = require('process');
+const argv = process.argv;
+let num = parseInt(argv[2], 10);
+if (!num) {
+  console.log('Missing size');
+}
+const i = num;
+while (num > 0) {
+  let temp = '';
+  for (let j = 0; j < i; j++) {
+    temp += 'X';
+  }
+  console.log(...temp);
+  num--;
+}
